9551e1fa3f1af26ece977afd4c6200f87608a12f
rKaushalya <ravinduk56@gmail.com>
2024-02-21 00:04:58 +0530

    data insert and delete from database

--- learn-jpa-and-hibernate/src/main/java/com/example/learnjpaandhibernate/course/jdbc/CourseJdbcCommandLineRunner.java
+++ learn-jpa-and-hibernate/src/main/java/com/example/learnjpaandhibernate/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -1,5 +1,6 @@
 package com.example.learnjpaandhibernate.course.jdbc;
 
+import com.example.learnjpaandhibernate.course.Course;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -11,6 +12,9 @@ public class CourseJdbcCommandLineRunner implements CommandLineRunner {
     private CourseJdbcRepository repository;
     @Override
     public void run(String... args) throws Exception {
-        repository.insert();
+        repository.insert(new Course(1,"Learn Aws Now","in28minutes"));
+        repository.insert(new Course(2,"Learn Azure Now","in28minutes"));
+        repository.insert(new Course(3,"Learn DevOps Now","in28minutes"));
+        repository.delete(1);
     }
 }
--- learn-jpa-and-hibernate/src/main/java/com/example/learnjpaandhibernate/course/jdbc/CourseJdbcRepository.java
+++ learn-jpa-and-hibernate/src/main/java/com/example/learnjpaandhibernate/course/jdbc/CourseJdbcRepository.java
@@ -1,5 +1,6 @@
 package com.example.learnjpaandhibernate.course.jdbc;
 
+import com.example.learnjpaandhibernate.course.Course;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -12,10 +13,19 @@ public class CourseJdbcRepository {
     private static String INSERT_QUERY =
             """
             insert into course (id, name, author)
-            values(1,'Learn Aws','in28minutes');
+            values(?,?,?);
             """;
 
-    public void insert(){
-        jdbcTemplate.update(INSERT_QUERY);
+    private static String DELETE_QUERY =
+            """
+            delete from course where id=?;
+            """;
+
+    public void insert(Course course){
+        jdbcTemplate.update(INSERT_QUERY,course.getId(),course.getName(),course.getAuthor());
+    }
+
+    public void delete(long id){
+        jdbcTemplate.update(DELETE_QUERY,id);
     }
 }
