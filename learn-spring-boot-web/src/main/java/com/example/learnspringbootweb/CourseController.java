e5036f9c6400f94a0e53c2658b328d07a9705af9
rKaushalya <ravinduk56@gmail.com>
2024-02-20 13:32:59 +0530

    Create simple rest api

---
+++ learn-spring-boot-web/src/main/java/com/example/learnspringbootweb/CourseController.java
@@ -0,0 +1,19 @@
+package com.example.learnspringbootweb;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class CourseController {
+
+    @RequestMapping("/course")
+    public List<Course> getCourseDetails(){
+        return Arrays.asList(
+          new Course(1,"Learn AWS","in28minutes"),
+          new Course(2,"Learn DevOps","in28minutes")
+        );
+    }
+}
